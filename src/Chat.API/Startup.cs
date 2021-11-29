667be87a5002a35350c3f1e62817874785e791a2
mehdihadeli <mehdi.hadeli@outlook.com>
2021-11-29 20:17:09 +0330

    .

--- src/Chat.API/Startup.cs
+++ src/Chat.API/Startup.cs
@@ -81,7 +81,7 @@ namespace Chat.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("docker"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
--- src/Chat.Web/Startup.cs
+++ src/Chat.Web/Startup.cs
@@ -50,7 +50,7 @@ namespace Chat.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("docker"))
             {
                 app.UseDeveloperExceptionPage();
             }
